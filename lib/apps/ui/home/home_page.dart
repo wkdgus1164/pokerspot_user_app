0db96488ab68dfc1f50509ca77b46b3f8a5039af
wkdgus1164 <wkdgus1164@naver.com>
2024-05-13 12:05:55 +0900

    build: ios 14

--- lib/apps/ui/home/home_page.dart
+++ lib/apps/ui/home/home_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pokerspot_user_app/apps/global/constants/assets.dart';
-import 'package:pokerspot_user_app/apps/global/theme/color_scheme.dart';
-import 'package:pokerspot_user_app/apps/ui/home/bottom_sheet/filter.dart';
 import 'package:pokerspot_user_app/apps/ui/home/views/list_view.dart';
 import 'package:pokerspot_user_app/apps/ui/home/views/location_view.dart';
 
@@ -30,23 +28,23 @@ class _HomePageState extends ConsumerState<HomePage> {
           HomeListView(),
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            showDragHandle: true,
-            isScrollControlled: true,
-            isDismissible: true,
-            enableDrag: true,
-            backgroundColor: colorGrey100,
-            builder: (context) {
-              return const HomeSearchFilterSheet();
-            },
-          );
-        },
-        icon: const Icon(Icons.tune_rounded),
-        label: const Text('상세 검색'),
-      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: () {
+      //     showModalBottomSheet(
+      //       context: context,
+      //       showDragHandle: true,
+      //       isScrollControlled: true,
+      //       isDismissible: true,
+      //       enableDrag: true,
+      //       backgroundColor: colorGrey100,
+      //       builder: (context) {
+      //         return const HomeSearchFilterSheet();
+      //       },
+      //     );
+      //   },
+      //   icon: const Icon(Icons.tune_rounded),
+      //   label: const Text('상세 검색'),
+      // ),
     );
   }
 }
