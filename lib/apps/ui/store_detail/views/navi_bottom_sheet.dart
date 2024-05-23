db689fb7870a5c1657574d83aa11d5e34bebce3c
wkdgus1164 <wkdgus1164@naver.com>
2024-05-23 15:52:16 +0900

    feat: apple map iOS 조건부 렌더링

--- lib/apps/ui/store_detail/views/navi_bottom_sheet.dart
+++ lib/apps/ui/store_detail/views/navi_bottom_sheet.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:logger/web.dart';
 import 'package:pokerspot_user_app/apps/global/constants/assets.dart';
 import 'package:pokerspot_user_app/apps/global/theme/color_scheme.dart';
 import 'package:pokerspot_user_app/apps/infra/third_party/kakao/navi/kakao_navi.dart';
@@ -66,24 +68,28 @@ class StoreDetailNaviBottomSheet extends StatelessWidget {
           },
         ),
         const Divider(color: colorGrey90),
-        ListTile(
-          leading: SvgPicture.asset(
-            Assets.appleMaps.path,
-            width: 40,
-            height: 40,
-          ),
-          title: const Text('위치 찾아보기'),
-          subtitle: const Text('애플 지도로 위치 찾아보기'),
-          onTap: () async {
-            final urlAppleMaps = 'https://maps.apple.com/?q=$y,$x';
+        if (Platform.isIOS) ...[
+          ListTile(
+            leading: SvgPicture.asset(
+              Assets.appleMaps.path,
+              width: 40,
+              height: 40,
+            ),
+            title: const Text('위치 찾아보기'),
+            subtitle: const Text('애플 지도로 위치 찾아보기'),
+            onTap: () async {
+              final urlAppleMaps = Uri.parse('https://maps.apple.com/?q=$y,$x');
 
-            if (Platform.isIOS && await canLaunchUrl(Uri.parse(urlAppleMaps))) {
-              await launchUrl(Uri.parse(urlAppleMaps));
-            } else {
-              throw 'Could not launch $urlAppleMaps';
-            }
-          },
-        ),
+              if (await canLaunchUrl(urlAppleMaps)) {
+                await launchUrl(urlAppleMaps);
+              } else {
+                Fluttertoast.showToast(msg: '애플 지도 앱을 설치 후 다시 시도해 주세요.');
+                Logger().e('Could not launch $urlAppleMaps');
+              }
+            },
+          ),
+          const Divider(color: colorGrey90),
+        ],
       ],
     );
   }
