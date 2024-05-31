6060754364198213c450ff33204a70e42603d6dd
wkdgus1164 <wkdgus1164@naver.com>
2024-05-31 23:14:42 +0900

    Update secret.dart

--- lib/secret/secret.dart
+++ lib/secret/secret.dart
@@ -7,7 +7,8 @@ enum Secret {
   // third party
   kakaoNativeKey('6beb52328c9577c22e6dafff95efe1d6'),
   kakaoJavaScriptKey('51c209ad19ea67073c407ab3fdeaed19'),
-  privacyPolicy('https://www.pokerspot.co.kr/privacy-policy'),
+  privacyPolicy(
+      'https://plip.kr/pcc/4b2f1cd7-81cb-41b4-9fbc-f9099f8f641d/privacy-policy'),
   ;
 
   const Secret(this.key);
