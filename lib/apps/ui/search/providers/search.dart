5e5a4147501cd1d3c6f499d2b4994c9a389110bf
wkdgus1164 <wkdgus1164@naver.com>
2024-06-25 01:18:34 +0900

    refactor: fetch stores api

--- lib/apps/ui/search/providers/search.dart
+++ lib/apps/ui/search/providers/search.dart
@@ -1,6 +1,8 @@
 import 'package:logger/logger.dart';
+import 'package:pokerspot_user_app/apps/global/constants/enums.dart';
 import 'package:pokerspot_user_app/apps/global/pagination/offset_pagination.dart';
 import 'package:pokerspot_user_app/apps/global/utils/extensions.dart';
+import 'package:pokerspot_user_app/apps/infra/api/stores/dto/stores_query.dart';
 import 'package:pokerspot_user_app/apps/infra/api/stores/stores_api.dart';
 import 'package:pokerspot_user_app/apps/infra/common/models/store.dart';
 import 'package:pokerspot_user_app/apps/ui/nearby/providers/geolocation_data.dart';
@@ -19,13 +21,22 @@ class SearchResult extends _$SearchResult {
   }
 
   Future<Models> _fetch() async {
-    final res = await ref.read(storesApiProvider).fetchStoresBySearch(
-          ref.read(geoLocationServiceProvider).latitude,
-          ref.read(geoLocationServiceProvider).longitude,
-          ref.read(searchKeywordProvider).keyword,
-          1,
-          PAGE_SIZE,
+    final res = await ref.read(storesApiProvider).fetchStores(
+          StoresQuery(
+            lat: ref.read(geoLocationServiceProvider).latitude,
+            lng: ref.read(geoLocationServiceProvider).longitude,
+            operationStatus: OperationStatus.ALL,
+            minOpenTime: "00:00",
+            maxOpenTime: "23:30",
+            gameType: GameType.ALL,
+            minEntryPrice: 0,
+            maxEntryPrice: 100,
+            searchText: ref.read(searchKeywordProvider).keyword,
+            page: 1,
+            perPage: PAGE_SIZE,
+          ),
         );
+
     Logger().i('res : ${res.data}');
 
     return WithOffsetPagination(
@@ -48,12 +59,20 @@ class SearchResult extends _$SearchResult {
 
     state = await AsyncValue.guard(
       () async {
-        final res = await ref.read(storesApiProvider).fetchStoresBySearch(
-              ref.read(geoLocationServiceProvider).latitude,
-              ref.read(geoLocationServiceProvider).longitude,
-              ref.read(searchKeywordProvider).keyword,
-              nextPage,
-              PAGE_SIZE,
+        final res = await ref.read(storesApiProvider).fetchStores(
+              StoresQuery(
+                lat: ref.read(geoLocationServiceProvider).latitude,
+                lng: ref.read(geoLocationServiceProvider).longitude,
+                operationStatus: OperationStatus.ALL,
+                minOpenTime: "00:00",
+                maxOpenTime: "23:30",
+                gameType: GameType.ALL,
+                minEntryPrice: 0,
+                maxEntryPrice: 100,
+                searchText: ref.read(searchKeywordProvider).keyword,
+                page: nextPage,
+                perPage: PAGE_SIZE,
+              ),
             );
 
         final data = res.data;
